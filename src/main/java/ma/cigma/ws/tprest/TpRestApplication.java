b8994f66ee28b37e53c50edbb9299ca88e733956
Noureddine Madkour <noredine.o3o@gmail.com>
2022-04-23 11:02:34 +0000

    tp2 war packaging

--- src/main/java/ma/cigma/ws/tprest/TpRestApplication.java
+++ src/main/java/ma/cigma/ws/tprest/TpRestApplication.java
@@ -2,12 +2,20 @@ package ma.cigma.ws.tprest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TpRestApplication {
+public class TpRestApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TpRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TpRestApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder
+                                                         application) {
+        return application.sources(TpRestApplication.class);
+    }
 
 }
